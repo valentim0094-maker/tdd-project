faf6a21ee24136e6e247083b87d47287ab1ad235
Tacio Valentim <valentim0094@gmail.com>
2026-08-27 11:30:41 -0300

    Criando projeto Django e subindo o servidor

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
+from selenium import webdriver 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+browser = webdriver.Firefox() 
+browser.get('http://127.0.0.1:8080') 
 
-assert 'Django' in browser.title
+assert 'install' in browser.title
