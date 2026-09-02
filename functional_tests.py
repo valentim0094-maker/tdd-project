9da4d3bf5c040f30d1e5b359b81c1e6c8ed66540
Tacio Valentim <valentim0094@gmail.com>
2026-09-02 20:31:22 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By  # <-- Import novo aqui!
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -15,24 +17,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
+        
+        # PROCURANDO O H1 COM A SINTAXE NOVA
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a entrar com um item TODO imediatamente
+        # PROCURANDO PELO ID COM A SINTAXE NOVA
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
 
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-        # Ela digita: "Estudar testes de unidade"
-
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
-
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
-
-        # Satisfeita, ela vai dormir
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Estudar testes funcionais' for row in rows)
+        )
 
 if __name__ == '__main__':
     unittest.main()
