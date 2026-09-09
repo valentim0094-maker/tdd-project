fdaa7b340e9dc7ff015e5cd13fea440636233357
Tacio Valentim <valentim0094@gmail.com>
2026-09-09 20:41:07 -0300

    Trabalhando com Multiplas Listas e API REST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
+    def test_can_start_a_list_for_one_user(self): 
         self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
@@ -40,7 +40,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Estudar testes funcionais')
         inputbox.send_keys(Keys.ENTER)
-        
         self.wait_for_row_in_list_table('1: Estudar testes funcionais')
         
         inputbox = self.browser.find_element(By.ID, 'id_new_item')  
@@ -49,3 +48,34 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.wait_for_row_in_list_table('1: Estudar testes funcionais')
         self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Estudar testes funcionais')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+
+        maria_list_url = self.browser.current_url
+        self.assertRegex(maria_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('1: Estudar testes funcionais', page_text)
+        self.assertNotIn('2: Estudar testes de unidade', page_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Comprar leite')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Comprar leite')
+
+        joao_list_url = self.browser.current_url
+        self.assertRegex(joao_list_url, '/lists/.+')
+        self.assertNotEqual(joao_list_url, maria_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Estudar testes funcionais', page_text)
+        self.assertIn('Comprar leite', page_text)
