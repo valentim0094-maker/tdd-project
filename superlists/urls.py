86c6a6f7b8c2c870b455f19987d7cac57ffb1f48
Tacio Valentim <valentim0094@gmail.com>
2026-09-09 20:56:27 -0300

    Implementando o Novo Projeto (Listas e Itens) - PARTE 2

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
