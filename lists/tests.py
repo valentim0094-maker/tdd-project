079af35a1cb0517f50c7c617531f6548affbaf44
Tacio Valentim <valentim0094@gmail.com>
2026-09-09 21:04:23 -0300

    Implementando o Novo Projeto (Listas e Itens) - PARTE 3

--- lists/tests.py
+++ lists/tests.py
@@ -48,3 +48,7 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
